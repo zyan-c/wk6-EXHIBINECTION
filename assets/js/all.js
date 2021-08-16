feecd6ca1a1d27db1f52abe3ee8bc841ba915504
zyan-c <zyanchang950@gmail.com>
2021-08-16 15:37:03 +0800

    Update 2021-08-16T07:37:01.678Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,11 @@
 "use strict";
 
+// 待刪？
 $(function () {
   console.log('Hello Bootstrap5');
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30
 });
 //# sourceMappingURL=all.js.map
