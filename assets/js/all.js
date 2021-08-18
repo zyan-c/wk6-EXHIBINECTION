672a06e2d499d2851763cfe803bcb042fb100b00
zyan-c <zyanchang950@gmail.com>
2021-08-18 13:06:28 +0800

    Update 2021-08-18T05:06:27.271Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,5 +7,14 @@ $(function () {
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30
-});
+}); // var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//     breakpoints: {
+//       768:{
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//       },
+//     }
+// });
 //# sourceMappingURL=all.js.map
